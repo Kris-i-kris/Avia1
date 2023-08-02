4b81d72a586f4fb30629a5704591243b23501d79
kris-i-kris <flare120491@gmail.com>
2023-08-02 14:18:18 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 565565;
+
+        int rub = 20;
+        //int 1mile = int rub;
+        int mile = ticketPrice / rub;
+
+        System.out.println("Начисленно миль: " + mile );
+        }
+    }
